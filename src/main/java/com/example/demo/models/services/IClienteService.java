10e1d88f0116d4fb239fac5f7edcf2f593521b15
Javier Tobar <javiertobarvera@gmail.com>
2020-08-08 17:24:51 -0500

    primer commit

---
+++ src/main/java/com/example/demo/models/services/IClienteService.java
@@ -0,0 +1,11 @@
+package com.example.demo.models.services;
+
+import java.util.List;
+
+import com.example.demo.models.entity.Cliente;
+
+public interface IClienteService {
+	
+	public List<Cliente> findAll();
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,1 @@
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Andres', 'Guzman', 'akjsdh@gmail.com', '2018-01-01');
