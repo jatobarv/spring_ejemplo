ef1f9266c71fc0cab186926370ad0c34148ddcb5
Javier Tobar <javiertobarvera@gmail.com>
2020-08-08 17:44:49 -0500

    Agrega CrossOrigin

--- src/main/java/com/example/demo/controllers/ClientRestController.java
+++ src/main/java/com/example/demo/controllers/ClientRestController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.models.entity.Cliente;
 import com.example.demo.models.services.IClienteService;
 
+@CrossOrigin(origins= {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClientRestController {
