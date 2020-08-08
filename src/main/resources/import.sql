4c2335e9fb86e638584069da966133c9fc7e8a29
Javier Tobar <javiertobarvera@gmail.com>
2020-08-08 17:32:47 -0500

    Agrega cliente

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,2 @@
 INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Andres', 'Guzman', 'akjsdh@gmail.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('juan', 'ogaks', 'ifwm@gmail.com', '2012-01-01');
